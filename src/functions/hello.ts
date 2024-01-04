59de91929e3530b4e36ff49a35433ea836954404
Vitor Sousa <v.sousa.cf@gmail.com>
2024-01-04 17:13:51 -0300

    increase types

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,4 +1,6 @@
-export async function handler(event) {
+import type { APIGatewayProxyEventV2 } from "aws-lambda";
+
+export async function handler(event: APIGatewayProxyEventV2) {
   return {
     statusCode: 200,
     body: JSON.stringify({
